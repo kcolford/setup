bb8faac66b7d20b6a133f5656c53c855c80c090f
Kieran Colford <kieran@rcafinnovation.ca>
2019-04-10 16:08:19 -0400

    changed to one github account

--- keys.sh
+++ keys.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 #keybase login kcolford
 
-cp sshkey.pub ~/.ssh/
-cp -n /dev/null ~/.ssh/sshkey || true
-chmod 600 ~/.ssh/sshkey
-keybase pgp decrypt < sshkey.enc > ~/.ssh/sshkey
+keybase pgp export -s | gpg --import
+
+keyname=id_rsa
+cp $keyname.pub ~/.ssh/
+cp -n /dev/null ~/.ssh/$keyname || true
+chmod 600 ~/.ssh/$keyname
+gpg < $keyname.enc > ~/.ssh/$keyname
